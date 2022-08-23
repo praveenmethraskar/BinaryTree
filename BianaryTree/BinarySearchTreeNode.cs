0118442578c74668c245a05099dda407771adf80
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-23 14:03:51 +0530

    creation of tree

---
+++ BianaryTree/BinarySearchTreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BianaryTree
+{
+    public class BinarySearchTreeNode<K>
+    {
+        public K Key;                           //Created One Key the values going to add passing it on both right and left
+        public BinarySearchTreeNode<K> Right;   //create variable for right side node
+        public BinarySearchTreeNode<K> Left;    //creates varibale for left side node
+
+        public BinarySearchTreeNode(K Key)
+        {
+            this.Key = Key;
+            Right = null;
+            Left = null;
+        }
+    }
+}
---
+++ BianaryTree/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BianaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree Problem");
+            BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+            ///UC1: Add the node in the tree
+
+            BinaryTree.AddNode(56);
+            BinaryTree.AddNode(30);
+            BinaryTree.AddNode(70);
+            BinaryTree.AddNode(22);
+            BinaryTree.AddNode(40);
+            BinaryTree.AddNode(11);
+            BinaryTree.AddNode(63);
+            BinaryTree.AddNode(67);
+            BinaryTree.AddNode(16);
+            BinaryTree.AddNode(60);
+            BinaryTree.AddNode(95);
+            BinaryTree.AddNode(65);
+            BinaryTree.AddNode(3); 
+
+
+        }
+    }
+}
