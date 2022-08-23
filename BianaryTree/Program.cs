63576d843fdcea35e077949c890c970c299351da
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-23 14:11:21 +0530

    searching tree

--- BianaryTree/Program.cs
+++ BianaryTree/Program.cs
@@ -30,6 +30,8 @@ namespace BianaryTree
             int Size = BinaryTree.GetSize();
             Console.WriteLine(Size);
 
+            bool result = BinaryTree.SearchKey(63);
+            Console.WriteLine(result);
         }
     }
 }
